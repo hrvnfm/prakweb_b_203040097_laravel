b99985e1c8de9a9a6ede206c482992f2fc9ad0e9
QueenAgella <hervinfakhrulm@gmail.com>
2022-10-26 20:10:57 +0700

    rev

--- myapp/resources/views/layouts/main.blade.php
+++ myapp/resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>UFM Blog | {{ $title }}</title>
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="{{ url('https://gamepedia.cursecdn.com/minecraft_gamepedia/a/a7/Heart.svg?version=dac1a46352ab4c25ad78a3cef0ceea86') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
