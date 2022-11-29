325d388ad61e4731a8f017de7e0601061629931e
QueenAgella <hervinfakhrulm@gmail.com>
2022-11-29 22:50:07 +0700

    update vid 23 rev

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Category;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategoryController;
+
 
 
 
@@ -65,4 +67,6 @@ Route::get('/dashboard', function () {
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug']) ->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', \App\Http\Controllers\AdminCategoryController::class)->except('show')->middleware('admin');
 
